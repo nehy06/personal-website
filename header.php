21d0fd3424c3f9b8f323170909e97e07246e9498
nehy <tereza.nehezova@gmail.com>
2025-05-07 21:39:31 +0200

    minor edit

--- header.php
+++ header.php
@@ -8,14 +8,13 @@
 <head>
     <meta charset="<?php bloginfo('charset'); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
 
     <!-- Google Tag Manager -->
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
     'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-KCGKMCT4');</script>
+    })(window,document,'script','dataLayer','GTM ID!!!');</script>
     <!-- End Google Tag Manager -->
 
     <?php wp_head(); ?>
@@ -23,7 +22,7 @@
 
 <body <?php body_class(); ?>>
     <<!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-KCGKMCT4"
+    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM ID!!!"
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
     
